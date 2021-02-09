97a20ab5724997fbbe00aca97d537e9d419d54dd
Bilal Zeineddine <bilazein302@ed.amdsb.ca>
2021-02-09 09:56:20 -0500

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace PlayGame
+{
+    partial class PlayGameLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayGameLabel));
+            this.StartButtonLabel = new System.Windows.Forms.Button();
+            this.CountdownLabel3 = new System.Windows.Forms.Label();
+            this.GoLabel = new System.Windows.Forms.Label();
+            this.GameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButtonLabel
+            // 
+            this.StartButtonLabel.AccessibleName = "StartButtonLabel";
+            this.StartButtonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButtonLabel.Location = new System.Drawing.Point(150, 85);
+            this.StartButtonLabel.Name = "StartButtonLabel";
+            this.StartButtonLabel.Size = new System.Drawing.Size(164, 38);
+            this.StartButtonLabel.TabIndex = 0;
+            this.StartButtonLabel.Text = "Start Game";
+            this.StartButtonLabel.UseVisualStyleBackColor = true;
+            this.StartButtonLabel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CountdownLabel3
+            // 
+            this.CountdownLabel3.AccessibleName = "CountdownLabel3";
+            this.CountdownLabel3.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountdownLabel3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.CountdownLabel3.Location = new System.Drawing.Point(147, 163);
+            this.CountdownLabel3.Name = "CountdownLabel3";
+            this.CountdownLabel3.Size = new System.Drawing.Size(170, 23);
+            this.CountdownLabel3.TabIndex = 1;
+            this.CountdownLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CountdownLabel3.Click += new System.EventHandler(this.CountdownLabel3_Click);
+            // 
+            // GoLabel
+            // 
+            this.GoLabel.AccessibleName = "GoLabel";
+            this.GoLabel.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoLabel.Location = new System.Drawing.Point(182, 126);
+            this.GoLabel.Name = "GoLabel";
+            this.GoLabel.Size = new System.Drawing.Size(100, 23);
+            this.GoLabel.TabIndex = 4;
+            this.GoLabel.Text = "GO!";
+            this.GoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.GoLabel.Click += new System.EventHandler(this.GoLabel_Click);
+            // 
+            // GameLabel
+            // 
+            this.GameLabel.AccessibleName = "GameLabel";
+            this.GameLabel.AutoSize = true;
+            this.GameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameLabel.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.GameLabel.Location = new System.Drawing.Point(181, 9);
+            this.GameLabel.Name = "GameLabel";
+            this.GameLabel.Size = new System.Drawing.Size(111, 25);
+            this.GameLabel.TabIndex = 5;
+            this.GameLabel.Text = "PlayGame";
+            this.GameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PlayGameLabel
+            // 
+            this.AccessibleName = "PlayGameLabel";
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(442, 248);
+            this.Controls.Add(this.GameLabel);
+            this.Controls.Add(this.GoLabel);
+            this.Controls.Add(this.CountdownLabel3);
+            this.Controls.Add(this.StartButtonLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PlayGameLabel";
+            this.Text = "PlayGame";
+            this.Load += new System.EventHandler(this.PlayGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButtonLabel;
+        private System.Windows.Forms.Label CountdownLabel3;
+        private System.Windows.Forms.Label GoLabel;
+        private System.Windows.Forms.Label GameLabel;
+    }
+}
+
---
+++ PlayGame/PlayGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; //allows the use of thread.sleep()
+using System.Media; //allows the use of soundplayer
+
+namespace PlayGame
+{
+    public partial class PlayGameLabel : Form
+    {
+        public PlayGameLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameLabel.Visible = false;
+            StartButtonLabel.Visible = false;
+            //create a sound player and load the alert.wav sound, then play it 
+
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.mixkit_simple_game_countdown_921);
+            //Begin countdown starting from 3, going until 1 then vanishing
+            alertPlayer.Play();
+            CountdownLabel3.Text = "Begin in 3...";
+            Refresh();
+            Thread.Sleep(1000);
+
+            CountdownLabel3.Text = "Begin in 2...";
+            Refresh();
+            Thread.Sleep(1000);
+
+            CountdownLabel3.Text = "Begin in 1...";
+            Refresh();
+            Thread.Sleep(1000);
+            //Change the sound from countdown to a gamesound loop, changing the background colour and displaying the "Go" text
+            SoundPlayer safariPlayer = new SoundPlayer(Properties.Resources.African_Safari_Loop);
+            safariPlayer.Play();
+            CountdownLabel3.Text = "";
+            GoLabel.Text = "GOOOO!!!";
+            this.BackColor = Color.Green;
+
+            GameLabel.ForeColor = Color.Purple;
+        }
+
+        private void CountdownLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoLabel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
